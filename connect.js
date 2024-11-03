a1f90ec645425f0a6c316d628aa3741d01a4d252
Hamza Mubin <hamzamubeen182@gmail.com>
2024-11-03 11:57:28 +0530

    hosting

--- connect.js
+++ connect.js
@@ -1,16 +1,21 @@
-import mysql from 'mysql'
+import mysql from 'mysql';
+import dotenv from 'dotenv';
 
-export const db=mysql.createConnection({
-    host : "localhost",
-    user:'root',
-    password:'Hamza@182',
-    database:'socialdb'
-})
+// Load environment variables from .env file
+dotenv.config();
+
+export const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+});
+
 db.connect((err) => {
     if (err) {
         console.error('Error connecting to MySQL:', err);
         return;
     }
-
     console.log('Connected to MySQL database successfully.');
 });
